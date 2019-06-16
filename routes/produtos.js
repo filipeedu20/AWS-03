9b22804708a0e159c0f6056d43252da50f6c7702
Filipe Eduardo <filipeedu.bh@gmail.comm>
2019-06-15 23:58:21 -0300

    Configurando rotas da aplicação

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const produtosController = require('./produtosController');
 const router = express.Router();
-
-
 /* GET product page. */
-router.get('/categoria', produtosController.get);
-
-router.get('/:id', produtosController.getById);
-router.post('/', produtosController.post); 
-
-router.delete('/carrinho/:id', produtosController.deleteID); 
-router.post('/carrinho/:id', produtosController.deleteID); 
-
+router.get('/categoria', produtosController.get); //retorna categorias dos produtos 
+router.get('/', produtosController.get); //retorna produtos 
+router.delete('/carrinho/:id', produtosController.deleteID);  //deleta item de carrinho  de compras 
+router.post('/carrinho/:id', produtosController.deleteID);  //insere item no carrinho de compras 
 
 module.exports = router;
